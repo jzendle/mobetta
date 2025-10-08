64762366cfb486af1d0d123f72f5407063bf8a58
JoeZ <joe@zentooling.com>
2025-10-08 10:17:12 -0600

    fixes per my yahoo_fin impl

--- mobetta.py
+++ mobetta.py
@@ -5,31 +5,50 @@ import pandas as pd
 import logging as l
 from concurrent.futures.thread import ThreadPoolExecutor
 
-l.basicConfig(filename='output.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=l.INFO)
-log = l.getLogger('main')
+l.basicConfig(
+    filename="output.log",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=l.INFO,
+)
+log = l.getLogger("main")
+
 
 def index_df_to_analysis(idx_name):
-    fname = idx_name + '.p' # ex. sp500.p
+    fname = idx_name + ".p"  # ex. sp500.p
     df = u.read_pickle(fname)
     tickers = df.ticker.unique()
     results_list = []
     for ticker in tickers:
-        ticker_df = df.query('ticker == @ticker')
+        ticker_df = df.query("ticker == @ticker")
         if ticker_df is None:
-            log.info('no data for ' + ticker)
+            log.info("no data for " + ticker)
             continue
         len_data = len(ticker_df)
         if len_data < 80:
-            log.info('not enough data for ' + ticker + ' - only ' + str(len_data) + ' days of data')
+            log.info(
+                "not enough data for "
+                + ticker
+                + " - only "
+                + str(len_data)
+                + " days of data"
+            )
             continue
         rank, current_close, vol, current_above_ma, gap = r.get_stats(ticker_df)
-        results_list.append({'ticker' : ticker, 'rank' : rank, 
-            'close': current_close , 'volatility' : vol, 'cls_gt_ma': current_above_ma, 'gap': gap})
+        results_list.append(
+            {
+                "ticker": ticker,
+                "rank": rank,
+                "close": current_close,
+                "volatility": vol,
+                "cls_gt_ma": current_above_ma,
+                "gap": gap,
+            }
+        )
 
     results_df = u.new_analysis_df(results_list)
-    results_df = results_df.sort_values(by=['rank'], ascending=False)
+    results_df = results_df.sort_values(by=["rank"], ascending=False)
 
-    analysis_fname = 'analysis-' + fname
+    analysis_fname = "analysis-" + fname
 
     u.pickle_file(results_df, analysis_fname)
 
@@ -37,92 +56,110 @@ def index_df_to_analysis(idx_name):
 
     return results_df
 
+
 def index_to_df_sync(index_name):
-    log.info('retrieving data for index: '+ index_name)
-    f_name= index_name + '.p'
-    tickers = s.index_ticker_fn(index_name)()
+    log.info("retrieving data for index: " + index_name)
+    f_name = index_name + ".p"
+    fxn = s.index_ticker_fn(index_name)
+    tickers = []
+    if fxn is not None:
+        tickers = fxn()
     # tickers = ['AAPL']
     df_list = []
     for ticker in tickers:
-        ticker_df = s.get_ticker_df( ticker)
+        ticker_df = s.get_ticker_df(ticker)
         if ticker_df is None:
-            log.info('unable to get data for ' + ticker)
+            log.info("unable to get data for " + ticker)
             continue
         df_list.append(ticker_df)
     # concat the list in one shot
     results_df = u.new_stock_df(df_list)
-    log.info('done.')
+    log.info("done.")
 
     u.pickle_file(results_df, f_name)
     return results_df
 
+
 def index_to_df(index_name):
-    log.info('retrieving data for index: '+ index_name)
-    fname = index_name + '.p'
-    tickers = s.index_ticker_fn(index_name)()
-    #tickers = ['AAPL']
+    log.info("retrieving data for index: " + index_name)
+    fname = index_name + ".p"
+    fxn = s.index_ticker_fn(index_name)
+    tickers = []
+    if fxn is not None:
+        tickers = fxn()
+    # tickers = ['AAPL']
     df_map_futures = {}
-    log.info('starting concurrent requests ...')
+    log.info("starting concurrent requests ...")
     with ThreadPoolExecutor(max_workers=50) as executor:
         for ticker in tickers:
             future = executor.submit(s.get_ticker_df, ticker)
             df_map_futures[future] = ticker
-    
 
-    log.info('submitted all jobs. waiting for responses ...')
+    log.info("submitted all jobs. waiting for responses ...")
     df_list = []
-    for future in df_map_futures: 
+    for future in df_map_futures:
         ticker_df = future.result()
         if ticker_df is None:
             ticker = df_map_futures[future]
-            log.info('unable to get data for ' + ticker)
+            log.info("unable to get data for " + ticker)
             continue
         df_list.append(ticker_df)
     # concat the list in one shot
     results_df = u.new_stock_df(df_list)
-    log.info('done.')
+    log.info("done.")
 
     u.pickle_file(results_df, fname)
     return results_df
 
+
 def create_portfolio(analysis_fname):
-    portfolio_name = 'portfolio-' + analysis_fname 
-    the_rest_name = 'not-portfolio-' + analysis_fname 
+    portfolio_name = "portfolio-" + analysis_fname
+    the_rest_name = "not-portfolio-" + analysis_fname
     df = u.read_pickle(analysis_fname)
     # keep the best 20% of the index - if a currently held stock falls out of this group - end of it!
     top_20_pct = int(len(df) / 5)
-    portfolio = df.query('gap == False and cls_gt_ma == True').head(top_20_pct) 
-    the_rest = df[df.ticker.isin(portfolio.ticker) == False] # keep track of ones not in portfolio too
+    portfolio = df.query("gap == False and cls_gt_ma == True").head(top_20_pct)
+    the_rest = df[
+        df.ticker.isin(portfolio.ticker) == False
+    ]  # keep track of ones not in portfolio too
 
     num_assets = 20
-    r.do_allocation(portfolio,num_assets)
+    r.do_allocation(portfolio, num_assets)
 
     # Create a Pandas Excel writer using XlsxWriter as the engine.
-    with pd.ExcelWriter('portfolio.xlsx') as writer:  # pylint: disable=abstract-class-instantiated
-        portfolio.to_excel(writer, sheet_name='portfolio')
-        the_rest.to_excel(writer, sheet_name='the rest')
-    print('sum of pct: {} sum of cost: {}'.format(portfolio.pct_alloc.sum(),
-    portfolio.cost.sum()))
+    with pd.ExcelWriter("portfolio.xlsx") as writer:  # pylint: disable=abstract-class-instantiated
+        portfolio.to_excel(writer, sheet_name="portfolio")
+        the_rest.to_excel(writer, sheet_name="the rest")
+    print(
+        "sum of pct: {} sum of cost: {}".format(
+            portfolio.pct_alloc.sum(), portfolio.cost.sum()
+        )
+    )
     u.pickle_file(portfolio, portfolio_name)
     u.pickle_file(the_rest, the_rest_name)
     # dump best part to screen
-    u.dump_file(portfolio_name,num=20)
-    
+    u.dump_file(portfolio_name, num=20)
+
 
 def usage():
-    print('usage: python mobetta.py [--pull | --analyze] # default is to pull and analyze ')
+    print(
+        "usage: python mobetta.py [--pull | --analyze] # default is to pull and analyze "
+    )
 
-if '__main__' == __name__:
+
+if "__main__" == __name__:
     import sys
 
-    pull = len(sys.argv) == 1 or sys.argv[1] == '--pull'
-    analyze = len(sys.argv) == 1 or sys.argv[1] == '--analyze'
+    usage()
+
+    pull = len(sys.argv) == 1 or sys.argv[1] == "--pull"
+    analyze = len(sys.argv) == 1 or sys.argv[1] == "--analyze"
     if not pull and not analyze:
         usage()
         exit(0)
 
-    #indxs = [ 'nasdaq' ,'sp500', 'dow']
-    indxs = [ 'sp500']
+    # indxs = [ 'nasdaq' ,'sp500', 'dow']
+    indxs = ["sp500"]
     if pull:
         [index_to_df(idx) for idx in indxs]
     if analyze:
--- stockapi.py
+++ stockapi.py
@@ -3,42 +3,39 @@ import datetime
 import re
 import logging as l
 
-log = l.getLogger('stockapi')
+log = l.getLogger("stockapi")
+
 
 def get_ticker_live(ticker):
     return si.get_live_price(ticker)
 
+
 def get_ticker_df(ticker, def_days=90):
-    days = def_days * 7/5  # convert into number of trading days
+    days = def_days * 7 / 5  # convert into number of trading days
     before = datetime.datetime.today() - datetime.timedelta(days=days)
     df = None
     try:
         # replace dot with dash - only occurs for BRK.B and BF.B
-        ticker = re.sub(r"([a-z])\.([a-z])", r"\1-\2",
-                        ticker, 0, re.IGNORECASE)
+        ticker = re.sub(r"([a-z])\.([a-z])", r"\1-\2", ticker, 0, re.IGNORECASE)
         df = si.get_data(ticker, start_date=before)
     except Exception as e:
-        log.debug(
-            'Exception {} getting data for {}'.format(
-                ticker, str(e)), exc_info=1)
+        log.info(f"Exception {e} getting data for {ticker}")
 
     return df
+
 
 def get_portfoilo_alloc(df):
     # return embelishished copy of input
     tmp = df.copy()
-    tmp['mkt_value'] = tmp.shares * tmp.ticker.apply(lambda x: get_ticker_live(x))
-    tmp['current_alloc'] = tmp.mkt_value / tmp.mkt_value.sum()
+    tmp["mkt_value"] = tmp.shares * tmp.ticker.apply(lambda x: get_ticker_live(x))
+    tmp["current_alloc"] = tmp.mkt_value / tmp.mkt_value.sum()
     return tmp
 
 
-
-ticker_to_idx = {
-    'sp500': 'SPY'
-}
+ticker_to_idx = {"sp500": "SPY"}
 
 
-def index_50_above_200(index='sp500'):
+def index_50_above_200(index="sp500"):
     ticker = ticker_to_idx[index]
     idx_df = get_ticker_df(ticker, 300)
     ma50 = idx_df.rolling(50).mean()[-1]
@@ -59,12 +56,14 @@ def get_dow_tickers():
 
 
 idx_dict = {
-    'sp500': get_sp500_tickers,
-    'nasdaq': get_nasdaq_tickers,
-    'dow': get_dow_tickers
+    "sp500": get_sp500_tickers,
+    "nasdaq": get_nasdaq_tickers,
+    "dow": get_dow_tickers,
 }
 
 
 def index_ticker_fn(idx_name):
-    log.info('index_ticker_fn: ' + idx_name)
-    return idx_dict.get(idx_name)
+    log.info("index_ticker_fn: " + idx_name)
+    tmp = idx_dict.get(idx_name)
+    if tmp is not None:
+        return tmp
